2e32261b0656ea6f0de4b971edd2610f50b91c18
hannadarwish <hannadarwish@gmail.com>
2023-06-12 15:07:09 -0700

    finished associations and validations

--- practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/app/models/item.rb
+++ practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/app/models/item.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id         :bigint           not null, primary key
+#  pokemon_id :bigint           not null
+#  name       :string           not null
+#  price      :integer          not null
+#  happiness  :integer          not null
+#  image_url  :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Item < ApplicationRecord
+  validates :name, :happiness, :image_url, presence: true
+  validates :name, length: {maximum: 255}
+  validates :price, numericality: { greater_than: 0 }
+
+
+
   belongs_to :pokemon
 end
--- practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/app/models/move.rb
+++ practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/app/models/move.rb
@@ -1,2 +1,20 @@
+# == Schema Information
+#
+# Table name: moves
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Move < ApplicationRecord
+    validates :name, length: {maximum: 255}, presence: true
+    validates :name, uniqueness: { message: "'%{value}' is already in use"}
+
+    has_many :poke_moves,
+    dependent: :destroy
+
+    has_many :pokemon,
+    through: :poke_moves
+
 end
